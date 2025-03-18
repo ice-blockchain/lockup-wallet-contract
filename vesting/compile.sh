12d2a1cf693c2f9058104c2f563fdb5e83c7da83
ice-charon <antoniy.shumanov@leftclick.io>
2025-03-18 15:17:41 +0200

    mainnet vesting parameters

--- vesting/compile.sh
+++ vesting/compile.sh
@@ -17,11 +17,11 @@ func=${CRYPTO_EXEC_PATH}/func
 inc=${CRYPTO_SRC_PATH}/fift/lib/:${CRYPTO_SRC_PATH}/smartcont/
 
 base_file=vesting-wallet # read private key from base_file.pk, save address in base_file.addr
-start_time=1742214072 # unix time start time of the vesting
-duration_time=10000 # duration of the vesting in seconds
-unlock_period=100 # period to unlock a portion of the vesting amount in seconds
+start_time=1736208000 # unix time start time of the vesting
+duration_time=141966000 # duration of the vesting in seconds
+unlock_period=7887000 # period to unlock a portion of the vesting amount in seconds
 cliff_period=0 # delay first unlock period by specific time in seconds
-vesting_amount=1000 # vesting amount in Grams
+vesting_amount=2356278478 # vesting amount in Grams
 
 rm vesting-lockup-wallet-code.fif
 $func -o vesting-lockup-wallet-code.fif -SPA ../stdlib.fc vesting-lockup-wallet.fc
